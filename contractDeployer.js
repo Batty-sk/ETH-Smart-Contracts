9f49d931600db0947e31008d889a1271624dc591
Batty-sk <saorav.skumar@gmail.com>
2024-08-15 17:03:16 +0530

    added the logic for connection with sepolia etherium network

--- contractDeployer.js
+++ contractDeployer.js
@@ -1,6 +1,16 @@
+require('dotenv').config();
 const Web3 = require('web3');
 
-// Define your private key and RPC endpoint
-const privateKey = process.env.PRIVATE_KEY; // Replace with your actual private key
+const privateKey = process.env.PRIVATE_KEY; 
 console.log(privateKey)
-const rpcUrl = 'https://ethereum-sepolia.core.chainstack.com/07cf9369e6ccb13a2ddacb25f511c28f'; // Your RPC endpoint
+const rpcUrl = 'https://ethereum-sepolia.core.chainstack.com/07cf9369e6ccb13a2ddacb25f511c28f'; 
+
+const web3 = new Web3.Web3(rpcUrl)
+console.log(web3.providers)
+web3.eth.getBlockNumber()
+  .then(blockNumber => {
+    console.log('Current block number:', blockNumber);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
