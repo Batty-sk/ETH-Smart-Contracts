05f074816cd8cbe1b7f2bb88ddd3f78fde24d553
Batty-sk <saorav.skumar@gmail.com>
2024-08-15 14:16:33 +0530

    installed web3 and initialized keys and rpc points

---
+++ contractDeployer.js
@@ -0,0 +1,6 @@
+const Web3 = require('web3');
+
+// Define your private key and RPC endpoint
+const privateKey = process.env.PRIVATE_KEY; // Replace with your actual private key
+console.log(privateKey)
+const rpcUrl = 'https://ethereum-sepolia.core.chainstack.com/07cf9369e6ccb13a2ddacb25f511c28f'; // Your RPC endpoint
